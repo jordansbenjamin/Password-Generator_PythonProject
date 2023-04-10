3d2be69fad9f6984fcf139fd62b1704c1cdeb904
jordansbenjamin <14327@coderacademy.edu.au>
2023-04-11 07:04:23 +1000

    Solved first part of password generator and works as expected

--- password_generator.py
+++ password_generator.py
@@ -10,3 +10,28 @@ print("Welcome to the PyPassword Generator!")
 nr_letters= int(input("How many letters would you like in your password?\n")) 
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+password = ""
+
+length_letters = len(letters)
+length_numbers = len(numbers)
+length_symbols = len(symbols)
+
+for letter in range(nr_letters):
+    random_letters = random.randint(0, length_letters - 1)
+    password += letters[random_letters]
+
+for number in range(nr_numbers):
+    random_numbers = random.randint(0, length_numbers - 1)
+    password += numbers[random_numbers]
+
+for symbol in range(nr_symbols):
+    random_symbols = random.randint(0, length_symbols - 1)
+    password += symbols[random_symbols]
+
+print(password)
+
+
+
+
+
